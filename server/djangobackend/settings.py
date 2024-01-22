d54913a7abf12648618b62b760f9cb8da52d0c27
sid161122 <bobatesiddhi@email.com>
2024-01-22 02:02:57 -0500

    created about us,contact page

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,9 +27,9 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['localhost','<Your app URL>']
+ALLOWED_HOSTS = ['localhost','https://bobatesiddhi-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
 
-CSRF_TRUSTED_ORIGINS = ['<Your app URL>']
+CSRF_TRUSTED_ORIGINS = ['https://bobatesiddhi-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
 
 
 # Application definition
