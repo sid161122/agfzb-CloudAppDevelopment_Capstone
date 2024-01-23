56c020006db9b1367d8a94a02dcf8cb3d8ea8ad2
sid161122 <bobatesiddhi@email.com>
2024-01-23 08:12:16 -0500

    create login,logout,reg

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,9 +27,9 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['localhost','https://bobatesiddhi-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS = ['localhost','https://bobatesiddhi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
-CSRF_TRUSTED_ORIGINS = ['https://bobatesiddhi-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS = ['https://bobatesiddhi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 
 # Application definition
